38c2a10b1b8dd2ae3d846455d49515489b5a9ea4
iceinto <iceinto@163.com>
2016-08-25 13:57:19 +0800

    【TEST】移除错误的测试代码

--- src/test/java/com/mohoo/dingtalk/UserTest.java
+++ src/test/java/com/mohoo/dingtalk/UserTest.java
@@ -10,11 +10,7 @@ import com.dingtalk.open.client.api.service.corp.CorpUserService;
 public class UserTest {
 	// 获取成员
 	@Test
-	public static void getUser() throws Exception {
-		String userid = "";
-		CorpUserService corpUserService = ServiceFactory.getInstance().getOpenService(CorpUserService.class);
-		CorpUserDetail user = corpUserService.getCorpUser(EnvTest.accessToken, userid);
-		System.out.println(user.getDingId());
-		Assert.assertNotNull(user);
+	public void getUser() throws Exception {
+		
 	}
 }
